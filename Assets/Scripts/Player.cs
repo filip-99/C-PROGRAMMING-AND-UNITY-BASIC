223a5c2cf99d0969b8120f6dbb7b90415e2ab310
Filip Radosavljević <filip6331@gmail.com>
2022-10-21 18:51:34 +0200

    Dodati osnovni parametri u za igrača u skripti Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Uvodimo javnu promenjivu
-    // 
-    public string name;
-    public int attack;
-    public float AttackSpeed;
+    // Uvodimo promenjive
+    // Prva stvar koju želimo jeste da se naša kocka kreće
+    public float speed = 10;
+
+    // Potrebno je odrediti i gravitaciju
+    public float gravity = 10;
+
+    // Potrebno je da imamo brzinu koju će mo da damo krutom telu igrača
+    // Brzina se povećava određeno vreme, kada se pokrenemo i ne prelazi zadatu brzinu tj. vrednost promenjive speed
+    public float maxVelocityChange = 10;
+
     private bool dead;
     public int health;
     private Transform PlayerTransform;
-    private GameObject Enemy;
 
     // Start is called before the first frame update
     void Start()
