0eff9df5a963a87bc62e9eccd41e5ad2adf4c278
Filip Radosavljević <filip6331@gmail.com>
2022-10-21 17:47:39 +0200

    Kreirana C# skripta sa nekim promenjivima različitih tipova

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Uvodimo javnu promenjivu
+    // 
+    public string name;
+    public int attack;
+    public float AttackSpeed;
+    private bool dead;
+    public int health;
+    private Transform PlayerTransform;
+    private GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
