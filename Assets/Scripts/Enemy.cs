e00cb66fc2567b3fa1b829ce85b73f43a1fa576a
Filip Radosavljević <filip6331@gmail.com>
2022-11-07 19:32:11 +0100

    Kreirana skripta i objekat neprijatelja

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Neprijatelji će imati više stanja (traženje, hodanje i napad)
+    //  Promenjiva tima enum sadrži listu stanja
+    public enum State
+    {
+        LOOKFOR,
+        GOTO,
+        ATTACK
+    };
+    // Trenutno stanje može da ima samo jednu vrednost iz liste enum
+    public State currentState;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
